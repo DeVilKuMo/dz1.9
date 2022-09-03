a0c9467ee8249ba735209b35010992a3178e7356
71 <devilkumo@mail.ru>
2022-09-03 12:06:26 +0300

    Дз сделал

--- src/Author.java
+++ src/Author.java
@@ -1,2 +1,26 @@
 public class Author {
+    private  final  String name ;
+    private  final  String surname ;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
 }
